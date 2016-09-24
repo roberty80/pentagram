72f9618a02124f17319bd78bd9f0316255c099eb
roberty80 <k.robinho_80@yahoo.es>
2016-09-24 17:06:29 +0300

    2

---
+++ react-2016/src/components/HomeLogin.js
@@ -0,0 +1,27 @@
+/**
+ * Created by Robert Kádár on 30/07/2016.
+ */
+"use strict";
+
+var React = require('react');
+var Router = require('react-router');
+var Link = Router.Link;
+
+
+var LoginForm = React.createClass({
+
+
+
+
+
+    
+
+
+
+
+
+
+
+    });
+
+module.exports = LoginForm;
---
+++ react-2016/src/components/singlePhoto.js
@@ -0,0 +1,3 @@
+/**
+ * Created by Robert Kádár on 02/08/2016.
+ */
